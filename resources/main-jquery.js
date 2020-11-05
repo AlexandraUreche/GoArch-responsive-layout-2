22cd2dc0119d832dedc98bf267507fc8cec0f5e7
AlexandraUreche <70063621+AlexandraUreche@users.noreply.github.com>
2020-11-05 22:00:42 +0200

    added fav functionality but it's not ready yet

--- resources/main-jquery.js
+++ resources/main-jquery.js
@@ -17,12 +17,15 @@ $(function () {
     getpropertyHTML = function (pictureObj) {
         return `<div class="image-gallery">
         <div class="image-wrapper" data-id=${pictureObj.id} style="background-image: url(assets/${pictureObj.imgUrl})">
+        <div class="title_and_favorite"> 
             <div class="first_title_image"><span>${pictureObj.description} </span><span>${pictureObj.place}</span></div>
+            <div class="favorites-images"></div>
+            </div>
             <div class="second_title_image">${pictureObj.architecturalDesign}</div>
         </div>
         `;
     };
-    
+
     for (let i = 0; i < picturesArr.length; i++) {
         let pictureObj = picturesArr[i],
             pictureHMTL = getpropertyHTML(pictureObj);
@@ -31,14 +34,21 @@ $(function () {
 
     const seeMoreProjectsBtn = $('.more_projects');
     seeMoreProjectsBtn.click(function () {
-            for (let i = 0; i < extraPicturesArr.length; i++) {
-                let pictureObj = extraPicturesArr[i],
-                    pictureHMTL = getpropertyHTML(pictureObj);
-                galleryWrapper.append(pictureHMTL);
-            }
-        
+        for (let i = 0; i < extraPicturesArr.length; i++) {
+            let pictureObj = extraPicturesArr[i],
+                pictureHMTL = getpropertyHTML(pictureObj);
+            galleryWrapper.append(pictureHMTL);
+        }
+
     });
-    $('form').on('submit', function(e) {
+
+    $('.favorites-images').click(function(){
+        console.log('click');
+        const id=$(this).parents('.image-wrapper').data('id');
+        $(this).toggleClass('selected');
+    });
+
+    $('form').on('submit', function (e) {
         e.preventDefault();
         console.log('click');
     });
