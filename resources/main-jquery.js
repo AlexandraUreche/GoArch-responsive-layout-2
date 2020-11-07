f2cff54c0c124dba1f580e93eb730c3c3f1495dd
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2020-11-07 13:22:04 +0200

    code review and fix for event delegation

--- resources/main-jquery.js
+++ resources/main-jquery.js
@@ -27,6 +27,7 @@ $(function () {
     };
     const seeMoreProjectsBtn = $('.more_projects');
     for (let i = 0; i < picturesArr.length; i++) {
+        // nu ai nevoie de if-ul asta aici, asta inseamna partea asta de for: i < picturesArr.length
         if (i < 6) {
             let pictureObj = picturesArr[i],
                 pictureHMTL = getpropertyHTML(pictureObj);
@@ -34,6 +35,7 @@ $(function () {
         }
     }
     seeMoreProjectsBtn.click(function () {
+        // pornesti de la 6 cu indexul si mergi pana la 5 aici? nu inteleg foarte bine
         for (let i = 6; i < picturesArr.length; i++) {
             let pictureObj = picturesArr[i],
                 pictureHMTL = getpropertyHTML(pictureObj);
@@ -42,7 +44,10 @@ $(function () {
     });
 
 
-    $('.favorites-images').click(function () {
+    // ai nevoie de event delegation aici pentru ca ai cod generat dinamic, iar la momentul in care DOM este incarcat prima data in browser, fisierele js inca ruleaza si risti sa nu ai HTML-ul generat ceea ce de fapt se si intampla
+    // din cauza aceeasta este ok sa te legi de un element ce deja exista in pagina inca de cand incarci pagina, .gallery-wrapper de exemplu
+    galleryWrapper.delegate('.favorites-images', 'click', function() {
+    // $('.favorites-images').click(function () {
         console.log('click');
         const id = $(this).parents('.image-wrapper').data('id');
         $(this).addClass('selected');
