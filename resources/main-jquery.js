644df00d07c4ccedbd57e5e4640f60afa12c8929
AlexandraUreche <70063621+AlexandraUreche@users.noreply.github.com>
2020-11-25 16:29:41 +0200

    finished the add the favorite part

--- resources/main-jquery.js
+++ resources/main-jquery.js
@@ -1,6 +1,13 @@
 $(function () {
+    let favoritesAdded = [];
     const seeMobileWrapperBtn = $('#seeMobileWrapperBtn');
     const closeMobileMenuBtn = $("#closeMobileMenuBtn");
+    const favoritesTotal = $('.favorites-number');
+    const favoritesHeaderIcon = $('.favorites-icon-wrapper'),
+        favoritesContentWrapper = $('.favorites-content-wrapper'),
+        removeFavoritesBtn = $('#removeFavoritesBtn'),
+        favoritesPictures = $('.favorites-pictures');
+    const galleryWrapper = $('.gallery-wrapper');
     seeMobileWrapperBtn.click(function () {
         console.log('click');
         $('.mobile-menu-wrapper').removeClass('hidden');
@@ -13,7 +20,7 @@ $(function () {
 
 
 
-    const galleryWrapper = $('.gallery-wrapper');
+
     getpropertyHTML = function (pictureObj) {
         return `<div class="image-gallery">
         <div class="image-wrapper" data-id=${pictureObj.id} style="background-image: url(assets/${pictureObj.imgUrl})">
@@ -27,34 +34,71 @@ $(function () {
     };
     const seeMoreProjectsBtn = $('.more_projects');
     for (let i = 0; i < picturesArr.length; i++) {
-        // nu ai nevoie de if-ul asta aici, asta inseamna partea asta de for: i < picturesArr.length
-        if (i < 6) {
-            let pictureObj = picturesArr[i],
-                pictureHMTL = getpropertyHTML(pictureObj);
-            galleryWrapper.append(pictureHMTL);
-        }
+        let pictureObj = picturesArr[i],
+            pictureHMTL = getpropertyHTML(pictureObj);
+        galleryWrapper.append(pictureHMTL);
+
     }
     seeMoreProjectsBtn.click(function () {
-        // pornesti de la 6 cu indexul si mergi pana la 5 aici? nu inteleg foarte bine
-        for (let i = 6; i < picturesArr.length; i++) {
-            let pictureObj = picturesArr[i],
+        for (let i = 0; i < extraPicturesArr.length; i++) {
+            let pictureObj = extraPicturesArr[i],
                 pictureHMTL = getpropertyHTML(pictureObj);
+
             galleryWrapper.append(pictureHMTL);
         }
     });
 
 
-    // ai nevoie de event delegation aici pentru ca ai cod generat dinamic, iar la momentul in care DOM este incarcat prima data in browser, fisierele js inca ruleaza si risti sa nu ai HTML-ul generat ceea ce de fapt se si intampla
-    // din cauza aceeasta este ok sa te legi de un element ce deja exista in pagina inca de cand incarci pagina, .gallery-wrapper de exemplu
-    galleryWrapper.delegate('.favorites-images', 'click', function() {
-    // $('.favorites-images').click(function () {
-        console.log('click');
+    galleryWrapper.delegate('.favorites-images', 'click', function () {
         const id = $(this).parents('.image-wrapper').data('id');
-        $(this).addClass('selected');
+        $(this).toggleClass('selected');
+        $('.favorites-icon-wrapper img').attr('src', 'assets/icon-heart-full.png')
+
+        if (jQuery.inArray(id, favoritesAdded) === -1) {
+            favoritesAdded.push(id);
+        } else {
+            favoritesAdded.splice(favoritesAdded.indexOf(id), 1);
+        }
+
+        if (favoritesAdded.length > 0) {
+            favoritesTotal.text(favoritesAdded.length);
+            favoritesTotal.show();
+           
+        } else {
+            favoritesTotal.hide();
+            $('.favorites-icon-wrapper img').attr('src', 'assets/icon-heart-full-white.png');
+        }
+    });
+    favoritesHeaderIcon.click(function () {
+        if (favoritesAdded.length > 0) {
+            const galleryArr=$.merge(picturesArr, extraPicturesArr);
+            galleryWrapper.hide(function () {
+                favoritesAdded.forEach(function (item) {
+                    for (let i = 0; i < galleryArr.length; i++) {
+                        let pictureObj =galleryArr[i];
+                        if (item === pictureObj.id) {
+                            let pictureHMTL = getpropertyHTML(pictureObj);
+                            favoritesPictures.append(pictureHMTL);
+                        }
+                    }
+                   
+                });
+                favoritesContentWrapper.show();
+                $('footer').hide();
+            });
+        }
+    });
+    removeFavoritesBtn.click(function() {
+        favoritesAdded = [];
+        favoritesTotal.hide();
+        $('.favorites-icon-wrapper img').attr('src', 'assets/icon-heart-full-white.png');
+        favoritesPictures.text('Your pictures have been removed. Please go back to choose others.');
+        favoritesPictures.addClass('removed-pictures');
+        
     });
 
+    
     $('form').on('submit', function (e) {
         e.preventDefault();
-        console.log('click');
     });
 });
